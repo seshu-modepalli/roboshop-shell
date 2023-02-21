a33e618b75d63c529de5a825ff02486a5e51c1d5
Seshabrahmam Modepalli <Seshabrahmam.Modepalli@omnitracs.com>
2023-02-21 12:07:31 +0530

    added frontend code

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,32 @@
-yum install nginx -y 
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+source common.sh
+
+print_head "installing nginx" 
+yum install nginx -y &>>${log_file}
+status_check $?
+
+
+print_head "Removing the old content"
+rm -rf /usr/share/nginx/html/* &>>{log_file}
+status_check $?
+
+print_head "Downloading the frontend source"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
+status_check $?
+
+print_head "Extracting downloaded frontend"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+status_check $?
+
+print_head "Copying Nginx config for roboshop"
+cp {code_dir}/config/nginx_roboshop.conf /etc/nginx/default.d/roboshop.conf &>>{log_file}
+status_check $?
+
+print_head "Enabling nginx"
 systemctl enable nginx 
+status_check $?
+
+print_head "Starting nginx"
 systemctl start nginx
+status_check $?
