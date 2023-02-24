d437baf6af671d947d4b1cdc4be0989d0f2dd1cf
Seshabrahmam Modepalli <Seshabrahmam.Modepalli@omnitracs.com>
2023-02-24 17:35:37 +0530

    added catalogue service code

--- common.sh
+++ common.sh
@@ -7,12 +7,133 @@ print_head(){
 }
 
 status_check(){
-
     if [ $1 -eq 0 ]; then 
     echo SUCCESS
     else
      echo "Read the log from log file ${log_file} for more information about error."
      exit 1
      fi
+}
 
+app_prereq_setup(){
+    print_head "Creating roboshop user"
+    id roboshop &>>${log_file}
+    if[$? -ne 0];then
+        useradd roboshop &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Create Application directory"
+    if[! -d /app];then
+        mkdir /app &>>${log_file}
+    fi
+    status_check $?
+
+    print_head " Deleting old content"
+    rm -rf /app/* &>>${log_file}
+    status_check $?
+    print_head "Downloading App content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    status_check $?
+
+    print_head "Extracting the app content"
+    cd /app
+    unzip /tmp/${component}.zip &>>${log_file}
+    status_check $?
+}
+
+schema_setup(){
+    if[ "$schema_type" = "mongo"];then
+        print_head "Copy MongoDB Repo files"
+        cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+        status_check $?
+
+        print_head "Install mongo client"
+        yum install mongodb-org-shell -y &>>${log_file}
+        status_check $?
+
+        print_head "Load schema"
+        mongo --host mongodb-dev.devopsaws.online </app/schema/${component}.js &>>${log_file}
+        status_check $?
+    elif[ "$schema_type" = "mysql"];then
+        print_head "Install MySQL Client"
+        yum install mysql -y &>>${log_file}
+        status_check $?
+
+        print_head "Load Schema"
+        mysql -h mysql-dev.devopsaws.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+        status_check $?
+    fi
+
+    }
+    systemd_setup(){
+        print_head "Copy SyatemD Service file"
+        cp ${code_dir}/config/{component}.service /etc/systemd/system/${component}.service &>>${log_file}
+        status_check $?
+
+        sed -i -e "s/ROBOSHOP_USER_PASSWORD/${roboshop_app_password}/" /etc/systemd/system/${component}.service &>>${log_file}
+
+        print_head "Reload SystemD"
+        systemctl daemon-reload
+        status_check $?
+
+        print_head "Enable ${component} Service"
+        systemctl enable ${component} &>>${log_file}
+        status_check $?
+
+        print_head "Start ${component} Service"
+        systemctl restart ${component} &>>${log_file}
+        status_check $?
+    }
+
+nodejs(){
+    print_head "Configuring nodejs repo"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+    status_check $?
+
+    print_head "Installing the nodejs"
+    yum install nodejs -y &>>${log_file}
+    status_check $?
+    
+    app_prereq_setup
+
+    print_head "Installing nodejs dependencies"
+    npm install &>>${log_file}
+    status_check $?
+
+    schema_setup
+
+    systemd_setup
+
+}
+java(){
+    print_head "Install maven"
+    yum install maven -y &>>${log_file}
+    status_check $?
+
+    app_prereq_setup
+
+    print_head "Download dependencies & package"
+    mvn clean package &>>${log_file}
+    mv target/{component}-1.0.jar ${component}.jar &>>${log_file}
+    status_check $?
+
+    schema_setup
+
+    systemd_setup
+}
+
+python(){
+
+    print_head "Installing python"
+    yum install python36 gcc python3-devel -y &>>${log_file}
+    status_check $?
+
+    app_prereq_setup
+
+    print_head "Download dependencies"
+    pip3.6 install -r requirements.txt &>>${log_file}
+    status_check $?
+
+    systemd_setup
 }
