8e90eb8482d210f311b2028f70030774c095aff7
Seshabrahmam Modepalli <Seshabrahmam.Modepalli@omnitracs.com>
2023-02-24 15:57:42 +0530

    added mongodb configuration

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
 source common.sh
 
-print_head("Setting up the mongodb repository")
+print_head "Setting up the mongodb repository"
 cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 status_check $?
 
-print_head("Installing mongodb")
+print_head "Installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 status_check $?
 
@@ -12,10 +12,10 @@ print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 status_check $?
 
-print_head("Enabling mongodb")
+print_head "Enabling mongodb" 
 systemctl enable mongod 
 status_check $?
 
-print_head("Starting mongodb")
+print_head "Starting mongodb"
 systemctl start mongod 
 status_check $?
